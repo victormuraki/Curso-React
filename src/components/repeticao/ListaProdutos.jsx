a6b17697cd80c09b93518de73fe2a8b1930de55c
Victor Muraki <vmuraki@MKMONDIAL.local>
2022-02-18 15:40:58 -0300

    terceiro commit

--- src/components/repeticao/ListaProdutos.jsx
+++ src/components/repeticao/ListaProdutos.jsx
@@ -1,17 +1,31 @@
 import React from "react";
 import produtos from '../../data/produtos'
+import './ListaProdutos.css'
 
 export default props => {
-    const produtosLI = produtos.map(produto => {
+    const produtosLI = produtos.map((produto, i) => {
         return (
-            <li key={produto.id}>
-                {produto.id}) {produto.nome} {produto.preco}
-            </li>
-            )
+            <tr key={produto.id} className={i % 2 === 0 ? 'Par' : 'Impar'}>
+                <td>{produto.id}</td>
+                <td>{produto.nome}</td>
+                <td>{produto.preco}</td>
+            </tr>
+        )
     })
     return (
-        <li style={{listStyle:'none'}}>
-            {produtosLI}
-        </li>
+        <div className="ListaProdutos">
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>Preco</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {produtosLI}
+                </tbody>
+            </table>
+        </div>
     )
 }
